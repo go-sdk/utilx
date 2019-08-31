b58b0eb2e2555058ab958fb05cbfdc26707fe102
starudream <justwangsheng@qq.com>
2019-08-31 14:08:42 +0800

    feat: add queue data structure

---
+++ ds/queue.go
@@ -0,0 +1,85 @@
+package ds
+
+import (
+	"container/list"
+	"reflect"
+	"sync"
+)
+
+type Queue struct {
+	data *list.List
+
+	enableLock bool
+	mu         *sync.Mutex
+}
+
+func NewQueue() *Queue {
+	return &Queue{
+		data: list.New(),
+		mu:   &sync.Mutex{},
+	}
+}
+
+func (queue *Queue) EnableLock(b bool) *Queue {
+	queue.enableLock = b
+	return queue
+}
+
+func (queue *Queue) Push(v interface{}) {
+	if queue.enableLock {
+		queue.mu.Lock()
+		defer queue.mu.Unlock()
+	}
+	queue.data.PushBack(v)
+}
+
+func (queue *Queue) PushN(v interface{}) {
+	vv := reflect.ValueOf(v)
+	if vv.Kind() != reflect.Slice || vv.Len() == 0 {
+		return
+	}
+	if queue.enableLock {
+		queue.mu.Lock()
+		defer queue.mu.Unlock()
+	}
+	for i := 0; i < vv.Len(); i++ {
+		queue.data.PushBack(vv.Index(i).Interface())
+	}
+}
+
+func (queue *Queue) Pop() interface{} {
+	if queue.enableLock {
+		queue.mu.Lock()
+		defer queue.mu.Unlock()
+	}
+	if queue.data.Len() == 0 {
+		return nil
+	}
+	return queue.data.Remove(queue.data.Front())
+}
+
+func (queue *Queue) PopN(n int) []interface{} {
+	if n <= 0 {
+		return nil
+	}
+	if queue.enableLock {
+		queue.mu.Lock()
+		defer queue.mu.Unlock()
+	}
+	if queue.data.Len() < n {
+		n = queue.data.Len()
+	}
+	vs := make([]interface{}, n)
+	for i := 0; i < n; i++ {
+		vs[i] = queue.data.Remove(queue.data.Front())
+	}
+	return vs
+}
+
+func (queue *Queue) Len() int {
+	if queue.enableLock {
+		queue.mu.Lock()
+		defer queue.mu.Unlock()
+	}
+	return queue.data.Len()
+}
---
+++ ds/queue_test.go
@@ -0,0 +1,31 @@
+package ds
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestNewQueue(t *testing.T) {
+	queue := NewQueue()
+	queue.EnableLock(true)
+	t.Logf("len0: %d", queue.Len())
+	for i := 1; i <= 5; i++ {
+		t.Logf("len1: %d", queue.Len())
+		t.Logf("push: %d", i)
+		queue.Push(i)
+		array := make([]int, rand.Intn(10))
+		for j := 0; j < len(array); j++ {
+			array[j] = rand.Intn(10)
+		}
+		t.Logf("push %d: %v", len(array), array)
+		queue.PushN(array)
+		t.Logf("len2: %d", queue.Len())
+		if i%2 == 0 {
+			t.Logf("pop: %v", queue.Pop())
+		} else {
+			t.Logf("pop %d: %v", i, queue.PopN(i))
+		}
+		t.Logf("len3: %d", queue.Len())
+	}
+	t.Logf("len0: %d", queue.Len())
+}
